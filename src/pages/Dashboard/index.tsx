7e77ebbc88e81aaa4b3f1d648e287f7d12982714
moacir.andretti <moacir.andretti@gmail.com>
2020-06-23 11:33:32 -0300

    Improve Dashboard page.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,62 @@
 import React from 'react'
 import logoImg from '../../assets/logo.svg'
-import { Title, Form } from './style'
+import { Title, Form, Repository } from './style'
+import { FiChevronRight } from 'react-icons/fi'
 
 const Dashboard: React.FC = () => {
 	return (
 		<>
 			<img src={logoImg} alt="GitHub Explorer" />
-			<Title>Explore repositório no GitHub</Title>
+			<Title>Explore repositórios no GitHub</Title>
 
 			<Form>
 				<input placeholder="Digite o nome do repositório" />
 				<button type="submit">Pesquisar</button>
 			</Form>
+
+			<Repository>
+				<a href="#">
+					<img
+						src="https://avatars1.githubusercontent.com/u/61529411?s=460&u=16cf6d51da44c338f89183b7d97e72a10fffa15a&v=4"
+						alt="Alt"
+					/>
+
+					<div>
+						<strong>Login System Address</strong>
+						<p>Simple CRUD system with JS, React JS and JSON Server</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+
+				<a href="#">
+					<img
+						src="https://avatars1.githubusercontent.com/u/61529411?s=460&u=16cf6d51da44c338f89183b7d97e72a10fffa15a&v=4"
+						alt="Alt"
+					/>
+
+					<div>
+						<strong>Login System Address</strong>
+						<p>Simple CRUD system with JS, React JS and JSON Server</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+
+				<a href="#">
+					<img
+						src="https://avatars1.githubusercontent.com/u/61529411?s=460&u=16cf6d51da44c338f89183b7d97e72a10fffa15a&v=4"
+						alt="Alt"
+					/>
+
+					<div>
+						<strong>Login System Address</strong>
+						<p>Simple CRUD system with JS, React JS and JSON Server</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+			</Repository>
 		</>
 	)
 }
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -20,6 +20,7 @@ export const Form = styled.form`
 		border: 0;
 		border-radius: 5px 0 0 5px;
 		color: #3a3a3a;
+		font-size: 18px;
 
 		&::placeholder {
 			color: #a8a8b3;
@@ -42,6 +43,56 @@ export const Form = styled.form`
 
 		&:hover {
 			background: ${shade(0.2, '#04d361')};
+		}
+	}
+`
+export const Repository = styled.div`
+	margin-top: 80px;
+	max-width: 700px;
+
+	a {
+		background: #fff;
+		border-radius: 8px;
+		width: 100%;
+		padding: 24px;
+		display: block;
+		text-decoration: none;
+		color: #a8a8b3;
+
+		display: flex;
+		align-items: center;
+		transition: transform 0.5s;
+
+		&:hover {
+			transform: translateX(15px);
+		}
+
+		& + a {
+			margin-top: 16px;
+		}
+
+		img {
+			width: 64px;
+			height: 64px;
+			border-radius: 50%;
+		}
+
+		div {
+			margin-left: 16px;
+			strong {
+				font-size: 20px;
+				color: #3d3d4d;
+			}
+			p {
+				font-size: 18px;
+				color: a8a8b3;
+				margin-top: 4px;
+			}
+		}
+
+		svg {
+			margin-left: auto;
+			color: #cbcbd6;
 		}
 	}
 `
